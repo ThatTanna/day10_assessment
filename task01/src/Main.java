a5a6fc3a517bf79f704b7b2567e159f48bc29c2f
Tanna <tanna1987@gmail.com>
2022-12-16 16:59:30 +0800

    day10_assessment

--- task01/src/Main.java
+++ task01/src/Main.java
@@ -5,20 +5,28 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        
+
+        // args[0] for .csv file | args[1] for template.txt
         String csvFile = args[0];
         String tmpFile = args[1];
 
+        // Map to store users (first row)
         Map<String, List<String>> users = new HashMap<>();
-        
-        BufferedReader br = new BufferedReader(new FileReader(csvFile));
+        List<String> allWords = new ArrayList<>();
 
+        // To read lines in .csv and .txt files
+        BufferedReader br = new BufferedReader(new FileReader(csvFile));
+        Scanner scanner = new Scanner(tmpFile);
+
+        // keys will be stored as array of string split by , first line
         String[] keys = br.readLine().split(",");
 
         String line;
+        String word;
         while ((line = br.readLine()) != null) {
             String[] values = line.split(",");
             String key = values[0];
@@ -28,9 +36,24 @@ public class Main {
             // System.out.println(", key: " + key + " , list: " + list);
         }
 
-        for (String i : keys) {
-            System.out.println(i);
+        while (scanner.hasNext()) {
+            word = scanner.next();
+            System.out.println(word);
+            allWords.add(word);
         }
 
+        while (scanner.hasNextLine()) {
+            line = scanner.nextLine();
+            for (String i : keys) {
+                System.out.println(i);
+            }
+        }
+
+        // Find <<key>> replace with values.
+
+        // for (int i = 0; i < allWords.size(); i++) {
+
+        // }
     }
+
 }
